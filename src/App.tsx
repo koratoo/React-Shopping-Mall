9712ba44ac1fd2e5cf56c35879e5cb04652feeb5
koratoo <ssyk2@naver.com>
2024-06-20 21:58:16 +0900

    불필요한 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+//App.tsx
+interface ProductType {
+  id: number;
+  name: string;
+  explanation: string;
+  price: number;
+}
+
 
 function App() {
+  const [products, setProducts] = useState<ProductType[]>([
+  {
+    id: 0,
+    name: "Galaxy Folder Phone X10",
+    explanation: '2025년 새로 출시될 갤럭시 폴더블 폰 X10을 미리 만나보실 수 있습니다.',
+    price: 2010000,
+  },
+]);
+
+  console.log(products);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        console.log("제출")
+        }}
+      >
+        <input type="text" placeholder="상품 이름" />
+        <input type="text" placeholder="상품 설명" />
+        <input type="number" placeholder="상품 가격" />
+        <input type="submit" placeholder="상품 만들기" />
+      </form>
+
+      {products.map((product) => (
+      <div key={product.id}>
+        <div>{product.id}</div>
+        <div>{product.name}</div>
+        <div>{product.price}</div>
+        <div>{product.explanation}</div>
+      </div>
+      ))}
+    </>
   );
 }
 
