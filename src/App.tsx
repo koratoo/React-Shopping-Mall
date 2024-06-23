5ccff1af3dcc731d4b51c3507a9d8854242f4fd0
koratoo <ssyk2@naver.com>
2024-06-23 11:06:03 +0900

    feat: update로직 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useRef, useState } from "react";
 import "./App.css";
 
-
 interface ProductType {
   id: number;
   name: string;
@@ -9,12 +8,61 @@ interface ProductType {
   price: number;
 }
 
+interface ProductItemProps {
+  product: ProductType;
+  onDelete: (id: number) => void;
+  onUpdate: (product: ProductType) => void;
+}
+
+const ProductItem = ({ product, onDelete, onUpdate }: ProductItemProps) =>{
+  const {id, name, price, explanation} = product;
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editName, setEditName] = useState(product.name);
+  const [explanation, setEditExplanation] = useState(product.explanation);
+  const [editPrice, setEditPrice] = useState(product.price);
+
+  return (
+    <div>
+      <div>{id}</div>
+      <div>{name}</div>
+      <div>{price}</div>
+      <div>{explanation}</div>
+
+      <button type="button" onClick={() => onDelete(id)}>
+        삭제하기
+      </button>
+
+      <button type="button" onClick={() => setIsEditMode((prev) => !prev)}>
+        수정하기
+      </button>
+
+      {isEditMode && (
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            onUpdate({
+              id,
+              name: editName,
+              price: editPrice,
+              explanation: setEditExplanation,
+            });
+          }}
+      >
+
+        
+      </form>
+    </div>
+  )
+      
+}
+
 function App() {
   const [products, setProducts] = useState<ProductType[]>([
     {
       id: 0,
       name: "Galaxy Folder Phone X10",
-      explanation: "2025년 새로 출시될 갤럭시 폴더블 폰 X10을 미리 만나보실 수 있습니다.",
+      explanation:
+        "2025년 새로 출시될 갤럭시 폴더블 폰 X10을 미리 만나보실 수 있습니다.",
       price: 2010000,
     },
   ]);
@@ -69,28 +117,25 @@ function App() {
       </form>
 
       <div className="product-list">
-        {products.map(({id, name, price, explanation}) => (
+        {products.map(({ id, name, price, explanation }) => (
           <div key={id} className="product-item">
             <div className="product-id">ID: {id}</div>
             <div className="product-name">이름: {name}</div>
             <div className="product-price">가격: {price}원</div>
             <div className="product-explanation">설명: {explanation}</div>
-            <button 
+            <button
               type="button"
               onClick={() => {
-                setProducts(products.filter((product) => product.id !== id))
+                setProducts(products.filter((product) => product.id !== id));
               }}
             >
               삭제하기
             </button>
           </div>
         ))}
-       
       </div>
-      
     </div>
   );
 }
 
 export default App;
-
